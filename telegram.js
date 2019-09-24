f4afdd92a6b5081a72b57b648a250eea9b6dcaaf
Nikita <slxxxix@gmail.com>
2019-09-24 12:50:42 +0200

    telegram handler

---
+++ telegram.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+
+class Telegram {
+  async constructor (telegramToken, botUrl) {
+    this.api = axios.create({
+      baseURL: `https://api.telegram.org/bot${telegramToken}/`,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    await this.setWebhook(botUrl)
+  }
+
+  sendMessage(to, text) {
+    return new Promise((resolve, reject) => {
+      this.api.post('sendMessage', {
+        text: text,
+        chatId: to
+      })
+      .then(response => resolve(response.data))
+      .catch(reason => reject(reason))
+    })
+  }
+
+  async setWebhook(botUrl) {
+    this.api.post('setWebhook', {
+      url: botUrl
+    })
+    .then(response => console.log(response.data))
+    .catch(reason => console.error(reason))
+  }
+}
+
+export default Telegram
