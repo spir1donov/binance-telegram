c11375092a6c1b5c60175edd4b0dbd4dcc6d8b51
Nikita <slxxxix@gmail.com>
2019-09-25 22:00:52 +0200

    revert it back to beautiful

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const Telegram = require('./telegram.js')
 const Binance = require('./binance.js')
 const app = express()
 const port = 4321
 const config = require('./local.js')
 
-const binance = new Binance(config)
+const telegram = new Telegram(config.telegramToken, config.botUrl)
+const binance = new Binance(config.binanceKey)
+
+function handleBinanceMessage (message) {
+  const event = message.e
+  let text
+
+  if (event === 'executionReport' || event === 'ListStatus') {
+    text = `Order ID: ${message.i} for ${message.s}
+    Side: ${message.S}, Type: ${message.o}
+    Price: ${message.p}, Quantity: ${message.q}
+    Current order status: ${message.X}`
+
+    console.log('Sending order update', text)
+    try {
+      telegram.sendMessage(config.telegramUser, text)
+    } catch (e) {
+      console.error('Error occured', e)
+    }
+  }
+}
 
 app.use(express.json({
   type: '*/json',
@@ -15,17 +36,19 @@ app.get('/', (req, res) => res.send(
   'This is a Telegram bot notification system for Binance. Request unknown.'
 ))
 
-// app.get('/test', async (req, res) => {
-//   try {
-//     const response = await telegram.sendMessage(config.telegramUser,
-//       req.query.test
-//     )
-//     text = `Message sent. API Response: ${JSON.stringify(response, null, 2)}`
-//   } catch (e) {
-//     text = `Error occured: ${JSON.stringify(e, null, 2)}`
-//   }
-//   console.log('GET /test result', text)
-//   res.send(text)
-// })
+app.get('/test', async (req, res) => {
+  try {
+    const response = await telegram.sendMessage(config.telegramUser,
+      req.query.test
+    )
+    text = `Message sent. API Response: ${JSON.stringify(response, null, 2)}`
+  } catch (e) {
+    text = `Error occured: ${JSON.stringify(e, null, 2)}`
+  }
+  console.log('GET /test result', text)
+  res.send(text)
+})
+
+binance.setMessageHandler(handleBinanceMessage)
 
 app.listen(port, () => console.log(`Bot listening on port ${port}!`))
--- telegram.js
+++ telegram.js
@@ -2,7 +2,30 @@ const axios = require('axios')
 
 class Telegram {
   constructor (telegramToken, botUrl) {
+    this.api = axios.create({
+      baseURL: `https://api.telegram.org/bot${telegramToken}/`
+    })
 
+    this.setWebhook(botUrl)
+  }
+
+  sendMessage(to, text) {
+    return new Promise((resolve, reject) => {
+      this.api.post('sendMessage', {
+        text: text,
+        chat_id: to
+      })
+      .then(response => resolve(response.data))
+      .catch(reason => reject(reason))
+    })
+  }
+
+  async setWebhook(botUrl) {
+    this.api.post('setWebhook', {
+      url: botUrl
+    })
+    .then(response => console.log(response.data))
+    .catch(reason => console.error(reason))
   }
 }
 
