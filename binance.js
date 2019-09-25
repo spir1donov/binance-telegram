1def89ed10d59ad552791e3491b91eab29d8b377
Nikita <slxxxix@gmail.com>
2019-09-25 14:54:49 +0200

    merge classed

--- binance.js
+++ binance.js
@@ -5,6 +5,10 @@ const Telegram = require('./telegram.js')
 class Binance {
   // constructor(binanceKey, telegramRecipient, telegramBot) {
   constructor(config) {
+    this.tg = axios.create({
+      baseURL: `https://api.telegram.org/bot${config.telegramToken}/`
+    })
+
     this.api = axios.create({
       baseURL: `https://api.binance.com/`,
       headers: {
@@ -19,6 +23,7 @@ class Binance {
     this.ws = null
 
     this.initWebSocket()
+    this.setWebhook(botUrl)
   }
 
   async initWebSocket() {
@@ -64,7 +69,7 @@ class Binance {
 
     if (message.e === 'executionReport' || message.e === 'ListStatus') {
       try {
-        const result = this.telegram.sendMessage(config.telegramUser, `Order ID: ${message.i} for ${message.s}
+        const result = this.sendMessage(config.telegramUser, `Order ID: ${message.i} for ${message.s}
         Side: ${message.S}, Type: ${message.o}
         Price: ${message.p}, Quantity: ${message.q}
         Current order status: ${message.X}`)
@@ -81,6 +86,25 @@ class Binance {
       .then(response => resolve(response.data.listenKey))
       .catch(reason => reject(reason))
     })
+  }
+
+  sendMessage(to, text) {
+    return new Promise((resolve, reject) => {
+      this.tg.post('sendMessage', {
+        text: text,
+        chat_id: to
+      })
+      .then(response => resolve(response.data))
+      .catch(reason => reject(reason))
+    })
+  }
+
+  async setWebhook(botUrl) {
+    this.tg.post('setWebhook', {
+      url: botUrl
+    })
+    .then(response => console.log(response.data))
+    .catch(reason => console.error(reason))
   }
 }
 
